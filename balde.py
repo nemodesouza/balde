040c0d12b824dbbce3b0b7430b8f83ff2dccb4ad
nemodesouza <53828046+nemodesouza@users.noreply.github.com>
2019-10-08 19:29:28 -0300

    Add files via upload
    Exercício para a disciplina Introdução à Programação Orientada a Objetos, montando uma classe chamada Balde, nos seguintes termos:
    Defina uma classe chamada Balde que represente um balde, um objeto usado para armazenar água. Defina a classe com as seguintes características:
    - ao ser criado, sabe-se a capacidade do balde (quantos litros cabem no balde) e o balde não deve conter água. 
    - método que faz com que o balde fique cheio d'água. método que faz com que o falde fique vazio (sem água). 
    - método que verifique se o balde está cheio. método que verifique se o balde está vazio. 
    - método que retorne a capacidade do balde. método que retorne quantos litros o balde contém.
    - método que o balde derrame o máximo do seu conteúdo em outro balde sem que haja derramamento de água.
    - método que o balde receba o máximo de conteúdo de outro balde sem que haja derramamento de água.

---
+++ balde.py
@@ -0,0 +1,36 @@
+class Balde:
+    def __init__ (self, capacidade):
+        self.__capacidade = capacidade
+        self.__quantidade = 0
+    
+    def fique_cheio(self):
+        self.__quantidade = self.__capacidade
+    
+    def fique_vazio(self):
+        self.__quantidade = 0
+
+    def esta_cheio(self):
+        self.__quantidade == self.__capacidade
+    
+    def esta_vazio(self):
+        self.__quantidade == 0
+
+    def retorne_capacidade(self):
+        return self.__capacidade
+    
+    def retorne_quantidade(self):
+        return self.__quantidade
+    
+    def derrame_em(self, outro_balde):
+        quantidade_restante = outro_balde.__capacidade - outro_balde.__quantidade
+        
+        if quantidade_restante > self.__quantidade:
+            outro_balde.__quantidade += self.__quantidade
+            self.__quantidade = 0
+        else:
+            outro_balde.__quantidade = outro_balde.__capacidade
+            self.__quantidade -= quantidade_restante
+
+    def receba_de(self, outro_balde):
+        outro_balde.derrame_em(self)
+
